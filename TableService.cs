4995bac98eed2a68cb2d36ff98c80ef80d30dd25
Connaire Myall <st10297849@vcconnect.edu.za>
2024-08-30 20:08:51 +0200

    Add project files.

---
+++ TableService.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Data.Tables;
+using System.Threading.Tasks;
+
+namespace ST10297849_CLDV6212_POE
+{
+    public class TableService
+    {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerProfiles");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerProfile profile)
+        {
+            await _tableClient.AddEntityAsync(profile);
+        }
+    }
+}
